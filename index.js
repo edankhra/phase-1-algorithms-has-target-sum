71c1e5ecc015439144a54b5f9873bbb27c10f548
Ekta <edankhra@gmail.com>
2023-05-28 18:20:55 -0400

    completed

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
-}
+  
+    // Create a set to store the complements of elements in the array
+    let complements = new Set();
+  
+    for (let i = 0; i < array.length; i++) {
+      const currentNum = array[i];
+      const complement = target - currentNum;
+  
+      // Check if the complement exists in the set
+      if (complements.has(complement)) {
+        return true;
+      }
+  
+      // Add the current number to the set
+      complements.add(currentNum);
+    }
+  
+    // No two numbers sum up to the target
+    return false;
+  }
+  
+  // Test cases
+  console.log(hasTargetSum([3, 8, 12, 4, 11, 7], 10)); // true
+  console.log(hasTargetSum([22, 19, 4, 6, 30], 25)); // true
+  console.log(hasTargetSum([1, 2, 5], 4)); // false
+  console.log(hasTargetSum([-7, 10, 4, 8], 3)); // true
+  console.log(hasTargetSum([1, 2, 3, 4], 5)); // true
+  console.log(hasTargetSum([2, 2, 3, 3], 4)); // true
+  console.log(hasTargetSum([4], 4)); // false
+
 
 /* 
   Write the Big O time complexity of your function here
